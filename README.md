6d454929816ec29739a5825d35d4b09a83aed2e3
Molikkidd <mzkidd5@gmail.com>
2021-11-04 12:21:34 -0700

    update: add navbar icons and links

--- README.md
+++ README.md
@@ -86,11 +86,23 @@ yarn add @heroicons/react
 
 `3` Import `Image` from `next/image` node module
 
+```js
+import Image from "next/image"
+```
 
-`4` Add your `Image` tag
+`4` Add your `Image` tag and src's for the images
 
+```js
+<Image
+  src="https://links.papareact.com/f90"
+  width={150}
+  height={40}
+  objectFit="contain"
+  className="cursor-pointer"
+  />
+```
 
-`5` setup next.js to connect images to your domain. Create a nextconfig.js file and add the following code 
+`5` setup next.js to connect images to your domain. Create a nextconfig.js file at the root of the folder and add the following code inside. 
 
 ```js
 module.exports = {
@@ -98,4 +110,42 @@ module.exports = {
         domains: ['links.papareact.com', 'fakestoreapi.com']
     }
 };
+```
+
+Then restart your server
+
+`6` Import the shopping, search and menu icons from hero icons
+
+```js
+import { MenuIcon, 
+        SearchIcon, 
+        ShoppingCartIcon } 
+        from "@heroicons/react/outline"
+```
+`7` then add your `SearchIcon` to the search div and style with tailwind. 
+
+```js
+  <SearchIcon className="h-12 p-4"/>
+```
+
+Use `hidden` to hide the search bar on small screens and show when adjusting to larger screens with flexbox
+
+`8` Add tailwind styling to input box
+
+`9` Add Icons to the right side of the navbar
+
+`10` Add links to navbars by adding a custom utility to your components using tailwind. Go to your styles/global.css and add the following code 
+
+```css
+/* how to create a custom utility class */
+@layer components {
+    .link {
+        @apply cursor-pointer hover:underline
+    }
+}
+```
+
+```text
+git add .
+git commit -m "update: add navbar icons and links"
 ```
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Image from "next/image"
+import { MenuIcon, 
+        SearchIcon, 
+        ShoppingCartIcon } 
+        from "@heroicons/react/outline"
 
 export default function Header() {
     return (
         <header>
-
             {/* top nav */}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
@@ -16,10 +19,27 @@ export default function Header() {
                     className="cursor-pointer"
                     />
                 </div>
-            </div>
-            {/* add search bar */}
-            <div>
-                <input type="text" />
+                {/* search bar */}
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+                    <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
+                    <SearchIcon className="h-12 p-4"/>
+                </div>
+                {/* Right Side Icons */}
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap"> 
+                    <div className="link">
+                        <p>Hello Moto</p>
+                        <p className="font-extrabold">Account & Lists</p>
+                    </div>
+                    <div className="link">
+                        <p>Returns</p>
+                        <p className="font-extrabold">& Orders</p>
+                    </div>
+                    <div className="link">
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="font-extrabold">Basket</p>
+                    </div>
+                </div>
+
             </div>
             {/* bottom nav */}
             <div>
